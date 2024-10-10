f5bebf37d8cbd249090ca8fb931769e9cd66e76f
Hyunwook Nam <hw0knam@gmail.com>
2024-10-11 04:19:23 +0900

    feat: 헤더/푸터 레이아웃 구현

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'selector',
 	theme: {
-		extend: {},
+		screens: {
+			'sm': '640px',
+			'md': '768px',
+			'lg': '920px',
+		},
+		extend: {
+			container: {
+				padding: '1rem',
+			},
+		},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
