7f1690d8bb496af00872782eecd7dbe9cfca4e00
Hyunwook Nam <hw0knam@gmail.com>
2024-11-27 00:42:41 +0900

    feat: 폰트 asset화

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,7 +7,7 @@ export default {
 			'lg': '768px',
 		},
 		fontFamily: {
-			'sans': 'Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif',
+			'sans': 'Pretendard Variable, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif',
 		},
 		extend: {
 			container: {
