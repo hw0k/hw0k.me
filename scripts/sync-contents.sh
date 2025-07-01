45d8120d753bbbeeb43e17d26a8cc009f9f4ce00
Hyunwook Nam <hw0knam@gmail.com>
2025-07-01 15:17:47 +0900

    chore: archive 폴더 제외

--- scripts/sync-contents.sh
+++ scripts/sync-contents.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-aws s3 sync s3://hw0k-blog-obsidian-vault ./src/contents/ --exclude "templates/*" --delete
+aws s3 sync s3://hw0k-blog-obsidian-vault ./src/contents/ --exclude "templates/*" --exclude "archives/*" --delete
